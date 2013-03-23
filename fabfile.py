10869dbfe0ad1b3412e182dba9637f78475ee0dd
Spencer Herzberg <spencer.herzberg@gmail.com>
2013-03-23 12:32:15 -0500

    adding reqs, fab, and applicaiton.py

---
+++ fabfile.py
@@ -0,0 +1,15 @@
+from packager import Deployment
+from fabric.api import env
+
+
+env.hosts = ['localhost:41022']
+
+
+def deb():
+    deploy = Deployment("wb", "flask-native-package", 'git@github.com:whelmingbytes/flask-native-package.git', build_user='vagrant')
+    for attr in dir(deploy):
+        if not attr.startswith('_'):
+            print attr, ':', getattr(deploy, attr)
+    print
+    deploy.prepare_python_app()
+    deploy.build_deb()
