c684e5e2ac6e959ede4e3865d3da94269cace74e
Spencer Herzberg <spencer.herzberg@gmail.com>
2013-04-03 11:30:41 -0500

    upgrading to use Parcel; fully builds a deployable deb

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,8 @@
-from fabric.api import *
+from fabric.api import task, env
 from parcel.deploy import Deployment
 from parcel import distro
+from parcel import tools
+
 
 env.app_name = 'flask-native-package'
 env.user = 'root'
@@ -8,6 +10,18 @@ env.user = 'root'
 
 @task
 def deb():
-    deploy = Deployment(env.app_name, build_deps=['python-virtualenv', 'python-pip'], base='/opt/webapps/wb/testapp', arch=distro.Ubuntu())
+    deploy = Deployment(
+        env.app_name,
+        build_deps=['python-virtualenv', 'python-pip'],
+        base='/opt/webapps/wb/flask-native-package',
+        arch=distro.Ubuntu(),
+        version="0.0.1"
+    )
+
+    deploy.add_preinst(['service %s stop' % env.app_name])
+    deploy.add_postinst(['service %s start' % env.app_name])
     deploy.prepare_app()
+
+    #this should be built into parcel, or needs an Upstart(Depployment)
+    tools.rsync([deploy.path+'/debian/'],deploy.root_path,rsync_ignore='.rsync-ignore')
     deploy.build_package()
